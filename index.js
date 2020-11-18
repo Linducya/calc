06ec03d7102c1b711e5e65276498bdb4e5a363ca
Linda Thomson <linducya@gmail.com>
2020-11-18 17:40:39 +0000

    Branching with switch statement

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-const readline =   require('readline-sync');
-// const response = readline.prompt();
+const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
+console.log('\nWelcome to the calculator!');
 console.log('=========================');
 
-console.log('Please enter the operator: ');
+console.log('\nPlease enter the operator: ');
 const operator = readline.prompt();
 
-console.log('Please enter the first number: ');
-const firstNumber = readline.prompt();
-// firstNumber = (+firstNumber);
+console.log('\nPlease enter the first number: ');
+const firstArgument = readline.prompt();
+const firstNumber = +firstArgument;
 
-console.log('Please enter the second number: ');
-const secondNumber = readline.prompt();
-// secondNumber = (+secondNumber);
+console.log('\nPlease enter the second number: ');
+const secondArgument = readline.prompt();
+const secondNumber = +secondArgument;
 
 let answer;
 
-// if (operator === '+') {
-//     answer = +firstNumber + +secondNumber;
-// } else if (operator === '-') {
-//     answer = +firstNumber - +secondNumber;
-// } else if (operator === '*') {
-//     answer = +firstNumber * +secondNumber;
-// } else { answer = +firstNumber / +secondNumber; }
+// Example using if and else
+/* if (operator === '+') {
+    answer = firstNumber + secondNumber;
+} else if (operator === '-') {
+    answer = firstNumber - secondNumber;
+} else if (operator === '*') {
+    answer = firstNumber * secondNumber;
+} else if (operator === '/') { 
+    answer = firstNumber / secondNumber;
+} else {
+    console.log('\n' + operator + ' is not a valid operator');
+} */
 
+// Example using switch
 switch (operator) {
     case '+':
-        answer = +firstNumber + +secondNumber;
+        answer = firstNumber + secondNumber;
         break;
     case '-':
-        answer = +firstNumber - +secondNumber;    
+        answer = firstNumber - secondNumber;    
         break;
     case '*':
-        answer = +firstNumber * +secondNumber;
+        answer = firstNumber * secondNumber;
         break;   
     case '/':
-        answer = +firstNumber / +secondNumber;
+        answer = firstNumber / secondNumber;
         break    
     default:
-        console.log(operator + ' is not a valid operator');
+        console.log('\n' + operator + ' is not a valid operator');
         break;
 }
 
-
-// const answer = +firstNumber operator= +secondNumber;
-console.log('The answer is: ' + answer);
+console.log('\nThe answer is: ' + firstNumber + operator + secondNumber + ' = ' + answer + '\n');
